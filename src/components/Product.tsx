ef2880a5846e5baab56e61b6b0968d08492dd01f
Medul_Hasan <medul.techvill@gmail.com>
2023-07-29 23:46:50 +0600

    add product in cart and place order

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, memo } from "react";
 import { ReducerAction, ReducerActionType } from "../context/CartProvider";
 import { ProductType } from "../context/ProductsProvider";
 
@@ -45,4 +45,19 @@ const Product = ({
     return content;
 };
 
-export default Product;
+function areProductsEqual(
+    { product: prevProduct, inCart: prevInCart }: PropsType,
+    { product: nextProduct, inCart: nextInCart }: PropsType
+) {
+    return (
+        Object.keys(prevProduct).every((key) => {
+            return (
+                prevProduct[key as keyof ProductType] ===
+                nextProduct[key as keyof ProductType]
+            );
+        }) && prevInCart === nextInCart
+    );
+}
+
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual);
+export default MemoizedProduct;
